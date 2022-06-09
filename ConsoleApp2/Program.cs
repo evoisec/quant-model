d8a938876550c2676bb3c45a1501401722529db0
evo <evo.eftimov@isecc.com>
2022-06-09 20:56:41 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QuantUtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            String input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input} {"Begins with uppercase? ",30}: " +
+                              $"{(QuantLibrary.StartsWithUpper(input) ? "Yes" : "No")}{Environment.NewLine}");
+            row += 3;
+        } while (true);
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
---
+++ quant-modles2/QuantLib.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantUtilityLibraries
+{
+    public static class QuantLibrary
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+
+
+        public static int[] calculateVAR(int days)
+        {
+            int[] var = new int[days];
+
+            for (int i = 0; i < var.Length; i++)
+            {
+                var[i] = i;
+            }
+
+            return var;
+        }
+    }
+}
